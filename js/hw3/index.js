b7cfdcca13aa1f1ed96beac071b89ce117d95198
Max Kamenev <max@dwt.com.ua>
2018-09-11 14:43:33 +0300

    HW update

---
+++ js/hw3/index.js
@@ -0,0 +1,26 @@
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+
+const isLoginValid = function (login) {return login.length >= 4 && login.length <= 16 ? true : false;};
+
+const isLoginUnique = function (allLogins, login) {return allLogins.includes(login) ? false : true;};
+
+const addLogin = function (login) {
+    let loginLength = isLoginValid(login);
+    if (loginLength === false) {
+        return console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    }
+    let checkLogin = isLoginUnique(logins, login);
+    if (checkLogin === false) {
+        return console.log('Такой логин уже используется!');
+    } else {
+        logins.push(login);
+        return console.log('Логин успешно добавлен!');
+    }
+};  
+
+// Вызовы функции для проверки
+addLogin('Ajax'); // 'Логин успешно добавлен!'
+addLogin('robotGoogles'); // 'Такой логин уже используется!'
+addLogin('Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin('jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
